fc8bc8fb98bf3ac8295bf566b1295e1233ed2a5b
Eric Brelsford <ebrelsford@gmail.com>
2014-07-07 18:44:55 -0400

    Cache GeoJSON

--- handthatfeeds/organizations/urls.py
+++ handthatfeeds/organizations/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.views.decorators.cache import cache_page
 
 from .views import (OrganizationDetail, OrganizationList, OrganizationPhotos,
                     OrganizationVideos)
@@ -8,5 +9,5 @@ urlpatterns = patterns('',
     url(r'(?P<pk>\d+)/photos/', OrganizationPhotos.as_view()),
     url(r'(?P<pk>\d+)/videos/', OrganizationVideos.as_view()),
     url(r'(?P<pk>\d+)', OrganizationDetail.as_view()),
-    url(r'', OrganizationList.as_view()),
+    url(r'', cache_page(60 * 60)(OrganizationList.as_view())),
 )
