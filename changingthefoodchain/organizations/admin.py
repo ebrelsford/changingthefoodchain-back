eca76d64312ca4495f6577104b9dbbb49dd5e1ba
Eric Brelsford <ebrelsford@gmail.com>
2014-09-15 11:57:33 -0400

    More usable org admin

--- changingthefoodchain/organizations/admin.py
+++ changingthefoodchain/organizations/admin.py
@@ -6,6 +6,13 @@ from .models import Organization, Sector, Type
 
 
 class OrganizationAdmin(ModerationAdmin):
+    fields = ('name',
+              ('address_line1', 'address_line2',),
+              ('city', 'state_province', 'country',),
+              ('email', 'phone',),
+              'site_url', 'mission',
+              ('sectors', 'types',),
+              'centroid',)
     list_display = ('name', 'address_line1', 'city', 'state_province',)
     list_filter = ('sectors', 'types')
     search_fields = ('name', 'address_line1', 'city', 'state_province',)
