fa321f63c3fdcc92f688ea6d7303e64bfe60ff7e
Eric Brelsford <ebrelsford@gmail.com>
2014-09-17 15:54:39 -0400

    Don't bother with ModerationAdmin

--- changingthefoodchain/changingthefoodchain/moderator.py
+++ changingthefoodchain/changingthefoodchain/moderator.py
@@ -4,5 +4,6 @@ from moderation.moderator import GenericModerator
 class StandardModerator(GenericModerator):
     auto_reject_for_anonymous = False
     bypass_moderation_after_approval = True
+    notify_user = False
     visibility_column = 'visible'
     visible_until_rejected = False
